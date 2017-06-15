5c3f230bd8489d1b165d14f27dcadd04f4a8d549
divya1694 <divyarai1694@gmail.com>
2017-06-16 00:11:00 +0530

    file upload

--- src/core/model.js
+++ src/core/model.js
@@ -7,4 +7,32 @@ export default class Model {
         this.reactions = []
         this.metabolites = []
 	}
+
+	getReactionById (id) {
+		const reaction = this.reactions.filter(reaction => reaction.id === id)
+		return reaction[0]
+	}
+
+	getGeneById (id) {
+		const gene = this.genes.filter(gene => gene.id === id)
+		return gene[0]
+	}
+
+	getMetaboliteById (id) {
+		const metabolite = this.metabolites.filter(metabolite => metabolite.id === id)
+		return metabolite[0]
+	}
+
+	addReactions (reactions) {
+		this.reactions = this.reactions.concat(reactions)
+	}
+
+	addMetabolites (metabolites) {
+		this.metabolites = this.metabolites.concat(metabolites)
+	}
+
+	addGenes (genes) {
+		this.genes = this.genes.concat(genes)
+	}
+
 }
