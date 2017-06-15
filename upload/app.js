86ed24052ae2e2850a0873104420620da216dc55
divya1694 <divyarai1694@gmail.com>
2017-06-15 22:25:46 +0530

    Second commit

---
+++ upload/app.js
@@ -0,0 +1,30 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+var multer  = require('multer')
+var upload = multer({ dest: 'public/uploads/' })
+
+var sbml=require('libsbml');
+
+
+var app=express();
+
+var data=[{item:'get milk'},{item:'walk dog'},{item:'kick'}];
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
+app.get('/',function (req, res) {
+    
+	res.sendFile(__dirname +'/index.html');
+   
+});
+
+
+
+app.post('/upload', upload.single('myimage'),  function (req, res, next) {
+    console.log('divya');
+	console.log(req.file);
+   
+});
+
+
+app.listen(3000);
